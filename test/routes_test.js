59181ada22e98299f5fe32f9548f4056d5e6951e
Thomas Siller (Home) <siller.thomas@gmx.net>
2015-04-16 19:38:16 +0200

    init routes_test

--- test/routes_test.js
+++ test/routes_test.js
@@ -5,9 +5,15 @@ var router = require('../routes/routes');
 
 describe('Routes Tests', function() {
 
-	/*it('should render index.ejs', function(done) {
+	it('should render index.ejs', function(done) {
 		request(router)
 			.get('/')
 			.expect(200, done);
-	});*/
+	});
+
+	it('should get all Todos of User', function() {
+		request(router)
+			.get('/services/todos')
+			.expect(200);
+	});
 });
